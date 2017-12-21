772fe14cd728d030d11ef86e4684ef3f56f54388
6mmario <mmario_93@hotmail.es>
2017-12-21 12:12:22 -0600

    xx
    xxx

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, Response
 from arbolBinario import aBinary
+from ListaCircular import listaCircular
+from NodoCircular import nodeCircular
 
-
+lista = listaCircular()
 
 app = Flask("Proyecto1")
 
-
+@app.route('/agregarLista', methods=['POST'])
+def agregarLista():
+        parametro = str(request.form['dato'])
+        parametro2 = str(request.form['dato1'])
+        node = nodeCircular(parametro,parametro2)
+        lista.insert(node)
+        lista.graficar()
+        return "Dato Agregado a Lista_Simple " + str(parametro) +' '+ str(parametro2) + "! \n"
 
 
 
