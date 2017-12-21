ed715fb4b5b2710b5cccc1dc52fb0878222fc517
6mmario <mmario_93@hotmail.es>
2017-12-21 12:05:25 -0600

    detalles

--- Python/ListaCircular.py
+++ Python/ListaCircular.py
@@ -1,12 +1,13 @@
 from NodoCircular import nodeCircular
 from graphviz import Digraph
-class listaCircular:
 
+
+class listaCircular:
     def __init__(self):
         self.first = None
         self.last = None
 
-    def insert(self,element):
+    def insert(self, element):
         if self.first == None:
             self.first = element
             self.last = element
@@ -19,21 +20,21 @@ class listaCircular:
             self.last.next = self.first
             self.first.prev = self.last
 
-    def search(self,nombre, pasword):
+    def search(self, nombre, pasword):
         aux = self.first
         while True:
-            if (aux.nombre == nombre and aux.pasword==pasword):
+            if (aux.nombre == nombre and aux.pasword == pasword):
                 return aux
             aux = aux.next
             if (aux == self.first):
                 return None
 
-    def delete(self,element):
+    def delete(self, element):
 
-        if  element == self.first:
-            self.first=self.first.next
+        if element == self.first:
+            self.first = self.first.next
             self.first.prev = self.last
-            self.last.next  = self.first
+            self.last.next = self.first
         elif element == self.last:
             self.last = self.last.prev
             self.last.next = self.first
@@ -41,28 +42,27 @@ class listaCircular:
         else:
             aux = self.first
             while (aux.next != self.first):
-                if  (aux == element):
+                if (aux == element):
                     aux.next.prev = aux.prev
                     aux.prev.next = aux.next
                 aux = aux.next
 
-
     def graficar(self):
-        g=Digraph('G',filename='graficaLista.gv')
+        g = Digraph('G', filename='graficaLista.gv')
         g.body.append('rankdir=LR')
-        g.attr('node',shape='box',color='blue')
+        g.attr('node', shape='box', color='blue')
         aux = self.first
         if aux == None:
             print "Lista vacia"
             g.edge(str('LISTA VACIA'))
         else:
-            print str(aux.nombre+" "+aux.pasword)
-            if self.last == aux:
-                g.body.append('\"'+str(aux.nombre)+'\n'+str(aux.pasword)+'\"')
-            else:
-                while aux.next != self.first:
-                    g.edge(str(aux.nombre),str(aux.next.nombre))
-                    aux = aux.next
-                    print str(aux.nombre+" "+aux.pasword)
-            g.format='png'
-            g.view()
+            print str(aux.nombre + " " + aux.pasword)
+            while True:
+                g.edge(str(aux.nombre), str(aux.next.nombre))
+                g.body.append('[dir=both]')
+                aux = aux.next
+                if (aux == self.first):
+                    break
+                print str(aux.nombre + " " + aux.pasword)
+        g.format = 'png'
+        g.view()
