4c282716bce71a95a39ae8e1f47034ce568650a4
6mmario <mmario_93@hotmail.es>
2017-12-20 23:04:59 -0600

    Inicio Proyecto

---
+++ Python/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, Response
+from arbolBinario import aBinary
+
+
+
+app = Flask("Proyecto1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
