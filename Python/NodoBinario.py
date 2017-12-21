18740b4f890d48ef634c98c4f4cbfa12b8a70dd0
6mmario <mmario_93@hotmail.es>
2017-12-21 12:32:26 -0600

    NO SE QUE HICE
    hHHAHAH

--- Python/NodoBinario.py
+++
@@ -1,9 +0,0 @@
-class nBinary:
-
-    def __init__(self, id = None, left=None , right= None):
-        self.id = id
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        return "%d" %(self.id)
