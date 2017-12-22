3725f821aa8fe8efc21b567ef2646843c559fa31
201504081 <josdiego77@gmail.com>
2017-12-21 18:10:05 -0600

    Parte JAVA
    Esta es la primera parte hecha en java

---
+++ ProyectoEDD/src/proyectoedd/ProyectoEDD.java
@@ -0,0 +1,15 @@
+
+package proyectoedd;
+
+/**
+ *
+ * @author Diego
+ */
+public class ProyectoEDD {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
