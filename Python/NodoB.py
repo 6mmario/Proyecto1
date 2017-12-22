beaf4ab371b911cbb8f409a794db01f7ccb46b72
201504081 <josdiego77@gmail.com>
2017-12-21 18:03:41 -0600

    LIMPIO

--- Python/NodoB.py
+++
@@ -1,9 +0,0 @@
-class nodeB:
-    llaves = [5]
-    ramas = [5]
-    artistas = None
-    n = None
-
-    def nodoLleno(self,actual):
-        return actual.artista == 4
-    
--- Python/NodoCola.py
+++
@@ -1,9 +0,0 @@
-class nodeCola:
-    def __init__(self, nombre = None, elementCancion = None, next = None, prev = None):
-        self.nombre = nombre
-        self.elementCancion = elementCancion
-        self.next = next
-        self.prev = prev
-
-    def __str__(self):
-        return "%s %s" %(self.nombre)
